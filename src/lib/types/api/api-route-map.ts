82af667ed4e768bc50260036f2c9292aa07ad25e
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-02-23 08:07:32 +0100

    fix: autocomplete for api routes

--- src/lib/types/api/api-route-map.ts
+++ src/lib/types/api/api-route-map.ts
@@ -1,5 +1,14 @@
 import type { Document, DocumentContentTemplate, DocumentMessage } from "../db/shared-types"
 
+type ApiDocumentsIdMessages = {
+  POST: { req: DocumentMessage; res: { messageId: string } }
+};
+
+type ApiTemplatesId = {
+  PUT: { req: DocumentContentTemplate; res: { updatedTemplateId: string } }
+  DELETE: { res: { deletedTemplateId: string } }
+}
+
 /**
  * Define a mapping of API routes to their expected request and response types. This will be used to provide type safety for api routes and the apiFetch function, ensuring that the correct request body is provided for each route and that the response is correctly typed.
  */
@@ -7,14 +16,14 @@ export interface ApiRouteMap {
   "/api/documents": {
     POST: { req: Document; res: { documentId: string } }
   }
-  [key: `/api/documents/${string}/messages`]: {
-    POST: { req: DocumentMessage; res: { messageId: string } }
-  }
+
+  [key: `/api/documents/${string}/messages`]: ApiDocumentsIdMessages
+  "api/documents/${string}/messages": ApiDocumentsIdMessages
+
   "/api/templates": {
     POST: { req: DocumentContentTemplate; res: { templateId: string } }
   }
-  [key: `/api/templates/${string}`]: {
-    PUT: { req: DocumentContentTemplate; res: { updatedTemplateId: string } }
-    DELETE: { res: { deletedTemplateId: string } }
-  }
+  
+  [key: `/api/templates/${string}`]: ApiTemplatesId
+  "api/templates/${string}": ApiTemplatesId
 }
