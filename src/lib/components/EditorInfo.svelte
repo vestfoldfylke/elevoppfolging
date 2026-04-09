2489b1133810310ae995a32f517946bee1cae118
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-04-09 08:46:43 +0200

    feat: EditorInfo component

---
+++ src/lib/components/EditorInfo.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { EditorData } from "$lib/types/db/shared-types";
+  import { prettifyDate, prettifyDateTime } from "$lib/utils/prettify-date";
+
+  type EditorInfoProps = {
+    modified: EditorData
+    created?: EditorData
+    timestamp?: boolean
+    modifiedIndicator?: boolean
+    style?: string
+  }
+
+  let { created, modified, timestamp = false, modifiedIndicator = false, style }: EditorInfoProps = $props()
+</script>
+
+<div class="ds-paragraph" data-size="xs" style={style || ""}>{created?.by.fallbackName ?? modified.by.fallbackName} {timestamp ? prettifyDateTime(created?.at ?? modified.at) : prettifyDate(created?.at ?? modified.at)}{ modifiedIndicator && modified.at > (created?.at ?? modified.at) ? " Redigert" : ""}</div>
