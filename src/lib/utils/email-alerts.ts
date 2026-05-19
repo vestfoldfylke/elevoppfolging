0988a7b74671ec066b228a1f3d5bf7a54de17913
Rune Moskvil Lyngås <runely@users.noreply.github.com>
2026-05-19 14:19:38 +0200

    fix: Anonymized email alerts (#84)

--- src/lib/utils/email-alerts.ts
+++ src/lib/utils/email-alerts.ts
@@ -13,32 +13,32 @@ export const generateEmailAlertReceivers = (receivers: string[]): EmailAlertRece
 
 export const generateEmailAlertBody = (documentId: string, student: CachedFrontendStudent, type: NewDbEmailAlert["type"]): NewDbEmailAlert["alertBody"] => {
   return {
-    body: generateEmailAlertHtmlBody(documentId, student._id, student.name, type),
-    subject: generateEmailAlertHtmlSubject(student.name, type)
+    body: generateEmailAlertHtmlBody(documentId, student._id, type),
+    subject: generateEmailAlertHtmlSubject(type)
   }
 }
 
-const generateEmailAlertHtmlBody = (documentId: string, studentId: string, studentName: string, type: NewDbEmailAlert["type"]): string => {
+const generateEmailAlertHtmlBody = (documentId: string, studentId: string, type: NewDbEmailAlert["type"]): string => {
   const url: string = `<a href="${env.WEB_APP_URL}/students/${studentId}?documentId=${documentId}">Elevoppfølging</a>`
 
   if (type === "DOCUMENT_CREATED") {
-    return `Nytt notat opprettet på elev ${studentName}. Gå hit for å se det nye notat: ${url}`
+    return `Det er opprettet et notat: ${url}`
   }
 
   if (type === "DOCUMENT_MESSAGE_CREATED") {
-    return `Oppdatering opprettet på notat for elev ${studentName}. Gå hit for å se oppdateringen: ${url}`
+    return `Oppdatering på eksisterende notat: ${url}`
   }
 
-  return `Ukjent notattype opprettet på elev. Gå hit for å sjekke det ut: ${url}`
+  return `Ukjent notattype opprettet: ${url}`
 }
 
-const generateEmailAlertHtmlSubject = (studentName: string, type: NewDbEmailAlert["type"]): string => {
+const generateEmailAlertHtmlSubject = (type: NewDbEmailAlert["type"]): string => {
   if (type === "DOCUMENT_CREATED") {
-    return `Notat opprettet på elev ${studentName}`
+    return "Nytt notat"
   }
 
   if (type === "DOCUMENT_MESSAGE_CREATED") {
-    return `Oppdatering opprettet på notat for elev ${studentName}`
+    return "Oppdatering på notat"
   }
 
   return "Ukjent notattype opprettet"
