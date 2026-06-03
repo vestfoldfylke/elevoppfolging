6cde62c96aa073751d59c4d91e31bf1d580d7708
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-06-03 19:19:06 +0200

    feat: added vite testing config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,30 @@
 import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  test: {
+    projects: [
+      {
+        extends: "./vite.config.ts",
+        mode: "vitest-client",
+        test: {
+          name: "client",
+          environment: "happy-dom",
+          include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
+          exclude: ["src/lib/server/**", "tests/server/**"]
+        },
+        resolve: { conditions: ["browser"] }
+      },
+      {
+        extends: "./vite.config.ts",
+        mode: "vitest-server",
+        test: {
+          name: "server",
+          environment: "node",
+          include: ["tests/server/**/*.{test,spec}.{js,ts}"]
+        }
+      }
+    ]
+  }
 })
