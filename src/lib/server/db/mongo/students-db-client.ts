d71cb1f916c46d61a3caed404675e74ac740123d
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2026-06-22 11:16:36 +0200

    chore: Use same metric name syntax - Manual students can now be both manual students and existing auto users...

--- src/lib/server/db/mongo/students-db-client.ts
+++ src/lib/server/db/mongo/students-db-client.ts
@@ -135,8 +135,8 @@ export class StudentsDbClient implements IStudentsDbClient {
 
     const mainSchoolNumber: string | undefined = manualStudent.studentEnrollments.find((enrollment: StudentEnrollment) => enrollment.mainSchool)?.school.schoolNumber
     const metricBody: MetricCount = {
-      name: "ManualStudent_Create",
-      description: "Number of manual students created"
+      name: "Student_Create",
+      description: "Number of students created or given manual enrollment"
     }
     const labels: MetricLabel[] = []
 
@@ -179,7 +179,7 @@ export class StudentsDbClient implements IStudentsDbClient {
     const mainSchoolNumber: string | undefined = student.studentEnrollments.find((enrollment: StudentEnrollment) => enrollment.mainSchool)?.school.schoolNumber
     const metricBody: MetricCount = {
       name: "Student_Update",
-      description: "Number of students updated"
+      description: "Number of students updated or given manual enrollment"
     }
     const labels: MetricLabel[] = []
 
