b4506258b45868ac84cf88c4aed58cf288a023e6
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-06-22 08:46:11 +0200

    feat: add optional banner info message controlled by env variable

--- src/lib/components/AppHeader.svelte
+++ src/lib/components/AppHeader.svelte
@@ -25,64 +25,79 @@
   }
 </script>
 
-<div class="header-container">
-  <div class="logo-container">
-    <img src={favicon32} alt="{page.data.APP_INFO.NAME} logo" />
-    <span class="app-title">{page.data.APP_INFO.NAME}</span>
-  </div>
-  <nav class="desktop-menu">
-    <ul class="desktop-menu-items">
-      {#each menuItems as menuItem}
-        <li>
-          <a class="ds-paragraph ds-focus" data-variant="default" data-size="md" class:active={page.url.pathname === menuItem.href || (menuItem.hrefStartsWith && page.url.pathname.startsWith(menuItem.hrefStartsWith))} href={menuItem.href}>{menuItem.name}</a>
-        </li>
-      {/each}
-    </ul>
-  </nav>
-  <nav class="menu-right">
-    <span
-      class="ds-avatar"
-      data-variant="circle"
-      data-initials="{page.data.authenticatedPrincipal.displayName.split(' ').map(n => n[0]).join('').toUpperCase()}"
-      role="img"
-      aria-label="small"
-      data-size="sm"
-    ></span>
-
-    <button
-      onclick={() => mobileMenuOpen = !mobileMenuOpen}
-      class="ds-button mobile-menu-button"
-      data-variant="tertiary"
-      type="button"
-      popoverTarget="hamburger-menu"
-    ><span class="material-symbols-outlined">{mobileMenuOpen ? 'close' : 'menu'}</span></button>
-    <div
-      class="ds-popover ds-dropdown"
-      id="hamburger-menu"
-      popover="manual"
-      data-placement="bottom-end"
-      data-variant="default"
-    >
-      <ul>
+<div class="app-header">
+  <div class="header-container">
+    <div class="logo-container">
+      <img src={favicon32} alt="{page.data.APP_INFO.NAME} logo" />
+      <span class="app-title">{page.data.APP_INFO.NAME}</span>
+    </div>
+    <nav class="desktop-menu">
+      <ul class="desktop-menu-items">
         {#each menuItems as menuItem}
           <li>
-            <a
-              href={menuItem.href}
-              class="ds-button"
-              data-variant="tertiary"
-          >
-            {menuItem.name}
-          </a>
-        </li>
-      {/each}
+            <a class="ds-paragraph ds-focus" data-variant="default" data-size="md" class:active={page.url.pathname === menuItem.href || (menuItem.hrefStartsWith && page.url.pathname.startsWith(menuItem.hrefStartsWith))} href={menuItem.href}>{menuItem.name}</a>
+          </li>
+        {/each}
       </ul>
+    </nav>
+    <nav class="menu-right">
+      <span
+        class="ds-avatar"
+        data-variant="circle"
+        data-initials="{page.data.authenticatedPrincipal.displayName.split(' ').map(n => n[0]).join('').toUpperCase()}"
+        role="img"
+        aria-label="small"
+        data-size="sm"
+      ></span>
+
+      <button
+        onclick={() => mobileMenuOpen = !mobileMenuOpen}
+        class="ds-button mobile-menu-button"
+        data-variant="tertiary"
+        type="button"
+        popoverTarget="hamburger-menu"
+      ><span class="material-symbols-outlined">{mobileMenuOpen ? 'close' : 'menu'}</span></button>
+      <div
+        class="ds-popover ds-dropdown"
+        id="hamburger-menu"
+        popover="manual"
+        data-placement="bottom-end"
+        data-variant="default"
+      >
+        <ul>
+          {#each menuItems as menuItem}
+            <li>
+              <a
+                href={menuItem.href}
+                class="ds-button"
+                data-variant="tertiary"
+            >
+              {menuItem.name}
+            </a>
+          </li>
+        {/each}
+        </ul>
+      </div>
+    </nav>
+  </div>
+
+  {#if page.data.APP_INFO.BANNER_INFO_MESSAGE}
+    <div class="info-banner-container">
+      <div class="info-banner">
+        <span class="material-symbols-outlined" aria-hidden="true">info</span>
+        <p class="ds-paragraph">{page.data.APP_INFO.BANNER_INFO_MESSAGE}</p>
+      </div>
     </div>
-  </nav>
+  {/if}
+
 </div>
 
 <style>
+  .app-header {
+    width: 100%;
+  }
   .header-container {
-		width: 100%;
+		flex: 1;
     max-width: var(--max-page-width);
     margin: 0 auto;
 		height: var(--header-height);
@@ -142,7 +157,6 @@
     gap: var(--ds-size-2);
   }
 
-
   @media (min-width: 1024px) {
     nav.desktop-menu {
       display: flex;
@@ -151,4 +165,19 @@
       display: none;
     }
   }
+
+  .info-banner-container {
+    background-color: var(--ds-color-info-background-tinted);
+    border-top: 1px solid var(--ds-color-neutral-border-subtle);
+    width: 100%;
+  }
+
+  .info-banner {
+    max-width: var(--max-page-width);
+    padding: var(--ds-size-3) var(--ds-size-4);
+    margin: 0 auto;
+    display: flex;
+    gap: var(--ds-size-2);
+  }
+
 </style>
