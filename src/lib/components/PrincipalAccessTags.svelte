54a8c8e4bc1627f4bc8931d4ae41726830b9a021
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-08-18 13:35:11 +0200

    fix: update PrincipalAccessTags to display strongest access and additional access in popover

--- src/lib/components/PrincipalAccessTags.svelte
+++ src/lib/components/PrincipalAccessTags.svelte
@@ -17,8 +17,57 @@
     }
     return school.name
   }
+
+  let strongestPrincipalAccess: PrincipalAccessForStudent | undefined = $derived.by(() => {
+    if (principalAccessForStudent.length === 0) {
+      return undefined
+    }
+    return principalAccessForStudent[0]
+  })
 </script>
 
-{#each principalAccessForStudent as principalAccess}
-  <PrincipalAccessTag source={principalAccess.source} name={`${ACCESS_TYPE_DISPLAY_NAMES[principalAccess.type]}${principalAccess.accessThroughResource ? ` ${principalAccess.accessThroughResource.name}` : ""} ved ${getSchoolName(principalAccess.schoolNumber)}`} />
-{/each}
+{#if strongestPrincipalAccess}
+  <PrincipalAccessTag source={strongestPrincipalAccess.source} name={`${ACCESS_TYPE_DISPLAY_NAMES[strongestPrincipalAccess.type]}${strongestPrincipalAccess.accessThroughResource ? ` ${strongestPrincipalAccess.accessThroughResource.name}` : ""} ved ${getSchoolName(strongestPrincipalAccess.schoolNumber)}`} />
+{/if}
+{#if principalAccessForStudent.length > 1}
+  {@const randomId = crypto.randomUUID()}
+  
+  <button class="ds-button custom-ds-button-badge" type="button" popoverTarget="additional-principal-access-{randomId}">+{principalAccessForStudent.length - 1}</button>
+  
+  <div class="ds-popover custom-ds-popover" id="additional-principal-access-{randomId}" popover="auto" data-placement="top" data-autoplacement="true">
+    {#each principalAccessForStudent.slice(1) as access}
+      <PrincipalAccessTag source={access.source} name={`${ACCESS_TYPE_DISPLAY_NAMES[access.type]}${access.accessThroughResource ? ` ${access.accessThroughResource.name}` : ""} ved ${getSchoolName(access.schoolNumber)}`} />
+      <br />
+    {/each}
+  </div>
+{/if}
+
+<style>
+  .custom-ds-button-badge {
+    --dsc-badge-padding: 0 calc(var(--ds-size-1) + var(--ds-size-1) / 2);
+    --dsc-badge-size: calc(var(--ds-size-3) + var(--ds-size-1) / 2);
+    --dsc-badge-top: inherit;
+    --dsc-badge-bottom: inherit;
+    --dsc-badge-left: inherit;
+    --dsc-badge-right: inherit;
+    vertical-align: middle;
+    -webkit-print-color-adjust: exact;
+    print-color-adjust: exact;
+
+    border-radius: var(--ds-border-radius-full);
+    display: inline-grid;
+    font-size: var(--ds-font-size-minus-1);
+    min-height: var(--dsc-badge-size);
+    min-width: var(--dsc-badge-size);
+    padding: var(--dsc-badge-padding);
+    place-items: center;
+    box-sizing: border-box;
+    line-height: var(--ds-line-height-sm);
+    width: -moz-fit-content;
+    width: fit-content;
+  }
+
+  .custom-ds-popover {
+    max-width: fit-content;
+  }
+</style>
